e0b5af51ed5151e70feddfca04d2f57f905d6134
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-08-31 16:48:36 +0500

    Saved the custom fields data in Database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected 
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:First_Name, :Last_Name, :email, :password)}
+  end
 end
