a1c9fd2b32719017d182e2364b17dfd89ccf6652
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-01 12:17:38 +0500

    Added associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :post
   root to: 'admins#index'
-  devise_for :admins
-  # resources :admins
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_901_064_418) do
+ActiveRecord::Schema.define(version: 20_220_901_070_537) do
   create_table 'admins', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -37,5 +35,7 @@ ActiveRecord::Schema.define(version: 20_220_901_064_418) do
     t.text 'body'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.integer 'admin_id'
+    t.index ['admin_id'], name: 'index_posts_on_admin_id'
   end
 end
